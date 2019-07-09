ead7df014e20d21e9c87cc4d4595939c0729a1cb
Andrew Scholz <yoitsscholzy@gmail.com>
2019-07-08 20:13:19 -0400

    update to styled component

--- src/components/header.js
+++ src/components/header.js
@@ -1,35 +1,37 @@
 import { Link } from "gatsby"
+import styled from "styled-components"
 import PropTypes from "prop-types"
 import React from "react"
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
+  <StyledHeader>
+    <Section>
+      <H1>
+        <StyledLink to="/">{siteTitle}</StyledLink>
+      </H1>
+    </Section>
+  </StyledHeader>
 )
+
+const StyledHeader = styled.header`
+  background: var(--primary);
+  margin-bottom: 1.45rem;
+`
+
+const Section = styled.div`
+  margin: 0 auto;
+  max-width: 960px;
+  padding: 1.45rem 1.0875rem;
+`
+
+const H1 = styled.h1`
+  margin: 0;
+`
+
+const StyledLink = styled(Link)`
+  color: var(--white);
+  text-decoration: none;
+`
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
