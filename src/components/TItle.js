b40170abeb31ce7a6895cc7083b247510da4eb80
Andrew Scholz <yoitsscholzy@gmail.com>
2019-07-08 22:27:00 -0400

    finish basic index page

--- src/components/TItle.js
+++ src/components/TItle.js
@@ -2,5 +2,17 @@ import styled from "styled-components"
 
 export default styled.h2`
   font-size: 2rem;
-  color: ${props => (props.white ? "var(--white)" : "var(--black)")};
+  text-align: center;
+  position: relative;
+  display: inline-block;
+  margin: 0 0 2.5rem 0;
+  ::after {
+    content: "";
+    height: 1px;
+    width: 100%;
+    background: var(--primary);
+    position: absolute;
+    bottom: -0.5rem;
+    left: 0;
+  }
 `
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,30 +1,161 @@
 import React from "react"
 import { graphql } from "gatsby"
+import styled from "styled-components"
+import Img from "gatsby-image"
 
 import Layout from "../components/layout"
 import HeroImage from "../components/HeroImage"
 import SEO from "../components/seo"
-import Title from "../components/TItle"
+import Title from "../components/Title"
+import Section from "../components/Section"
+import Icon from "../components/Icon"
+import ContactForm from "../components/ContactForm"
+
+import { FaClock, FaMapMarkerAlt } from "react-icons/fa"
+import { domainToASCII } from "url"
 
 export const data = graphql`
   query MyQuery {
     file(name: { eq: "index-banner" }) {
       childImageSharp {
         fluid {
-          src
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+    site {
+      siteMetadata {
+        address
+        city
+        pastor
+      }
+    }
+
+    ben: file(name: { eq: "team-ben" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid
         }
       }
     }
   }
 `
 
-const IndexPage = ({ data }) => (
-  <Layout>
-    <SEO title="Home" />
-    <HeroImage image={data.file.childImageSharp.fluid} full></HeroImage>
+const getAge = birthday => {
+  const diff = Date.now() - birthday.getTime()
+  const ageDiff = new Date(diff)
 
-    <Title>Welcome to the homepage</Title>
-  </Layout>
-)
+  return Math.abs(ageDiff.getUTCFullYear() - 1970)
+}
+
+const IndexPage = ({ data, className }) => {
+  const abelAge = getAge(new Date(2015, 4, 23))
+  const cohenAge = getAge(new Date(2017, 4, 3))
+
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <HeroImage image={data.file.childImageSharp.fluid} full>
+        Welcomed
+        <br />
+        Wanted
+        <br />
+        Loved
+      </HeroImage>
+      <Section large>
+        <Title>
+          For Richmond.
+          <br />
+          For You.
+        </Title>
+        <InfoContainer>
+          <StyledIcon icon={FaClock} className={className} />
+          <H3>{`Sundays at 9:30 & 10:45a`}</H3>
+        </InfoContainer>
+        <InfoContainer>
+          <StyledIcon icon={FaMapMarkerAlt} className={className} />
+          <H3>
+            {data.site.siteMetadata.address}, {data.site.siteMetadata.city}
+          </H3>
+        </InfoContainer>
+      </Section>
+      <Section dark>
+        <Title>Our Pastor</Title>
+        <PastorContainer>
+          <Avatar fluid={data.ben.childImageSharp.fluid} />
+          <PastorInfo>
+            <PastorName>{data.site.siteMetadata.pastor}</PastorName>
+            <PastorDescription>
+              {`Ben joined Madison Hills in 2019. He loves his wife Whitney, and his two boys Abel, ${abelAge}, and Cohen, ${cohenAge}. He is also a big fan of the Cincinnati Reds, Chipotle, and The Office.`}
+            </PastorDescription>
+          </PastorInfo>
+        </PastorContainer>
+      </Section>
+      <Section>
+        <Title>Contact Us</Title>
+        <ContactForm />
+      </Section>
+    </Layout>
+  )
+}
+
+const InfoContainer = styled.div`
+  display: flex;
+  align-items: center;
+  :first-of-type {
+    margin-bottom: 0.75rem;
+  }
+`
+
+const StyledIcon = styled(Icon)`
+  font-size: 2rem;
+  color: var(--primary);
+  margin-right: 20px;
+`
+
+const H3 = styled.h3`
+  font-size: 1.25rem;
+  margin: 0;
+`
+
+const PastorContainer = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  max-width: 600px;
+  @media (min-width: 577px) {
+    flex-direction: row;
+  }
+`
+
+const Avatar = styled(Img)`
+  height: 200px;
+  width: 200px;
+  min-width: 200px;
+  border-radius: 50%;
+  margin: 0 0 2rem 0;
+  @media (min-width: 577px) {
+    margin: 0 2rem 0 0;
+  }
+`
+
+const PastorInfo = styled.div`
+  flex-shrink: 1;
+  text-align: center;
+  @media (min-width: 577px) {
+    text-align: left;
+  }
+`
+
+const PastorName = styled.h4`
+  color: var(--primary);
+  margin: 0;
+  font-size: 1.5rem;
+  text-transform: uppercase;
+`
+
+const PastorDescription = styled.p`
+  margin-bottom: 0;
+`
 
 export default IndexPage
