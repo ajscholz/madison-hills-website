de87c7de06883b0ba68b20394e7753218d7e6a47
Andrew Scholz <yoitsscholzy@gmail.com>
2019-07-10 16:32:06 -0400

    change give link to external

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -10,9 +10,20 @@ const Navigation = () => {
       <StyledList>
         {links.map(link => (
           <StyledListItem key={link.name}>
-            <StyledLink to={link.path} alt={link.name}>
-              {link.name}
-            </StyledLink>
+            {link.name !== "give" ? (
+              <StyledLink to={link.path} alt={link.name}>
+                {link.name}
+              </StyledLink>
+            ) : (
+              <StyledExtLink
+                as="a"
+                href={link.path}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {link.name}
+              </StyledExtLink>
+            )}
           </StyledListItem>
         ))}
       </StyledList>
@@ -40,6 +51,18 @@ const StyledListItem = styled.li`
 `
 
 const StyledLink = styled(Link)`
+  text-transform: capitalize;
+  font-size: 1.5rem;
+  transition: var(--mainTransition);
+  &:hover {
+    color: var(--primary);
+  }
+  @media (min-width: 663px) {
+    font-size: 1rem;
+  }
+`
+
+const StyledExtLink = styled(StyledLink)`
   text-transform: capitalize;
   font-size: 1.5rem;
   transition: var(--mainTransition);
--- src/utils/links.js
+++ src/utils/links.js
@@ -13,6 +13,7 @@ export default [
   },
   {
     name: "give",
-    path: "/give",
+    path:
+      "https://forms.ministryforms.net/viewForm.aspx?formId=f9334eba-aa63-459a-a85c-218f77216694",
   },
 ]
