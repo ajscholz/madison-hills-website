bf8caf4858dc5854ea8d3387a8ac82473cb4f471
Andrew Scholz <yoitsscholzy@gmail.com>
2019-07-26 08:49:57 -0400

    finish event card functionality

--- src/components/EventCard.js
+++ src/components/EventCard.js
@@ -1,43 +1,71 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
-import BackgroundImage from 'gatsby-background-image';
-import { useSpring, animated as a } from 'react-spring';
+import { useSpring, animated } from 'react-spring';
+import Img from 'gatsby-image';
 
-const EventCard = ({ image, title }) => {
-  const [flipped, setFlipped] = useState(false);
+const EventCard = props => {
+  const [flipped, set] = useState(false);
   const { transform } = useSpring({
     transform: `perspective(600px) rotateY(${flipped ? 180 : 0}deg)`,
     config: { mass: 5, tension: 500, friction: 80 },
   });
-  console.log(image);
-  const backgroundFluidImageStack = [
-    image,
-    `linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6))`,
-  ].reverse();
   return (
-    <StyledCard onClick={() => setFlipped(state => !state)}>
-      <CardFront fluid={backgroundFluidImageStack} style={{ transform }}>
-        <Text>{title}</Text>
+    <StyledDiv onClick={() => set(state => !state)}>
+      <CardFront style={{ transform }}>
+        <Img
+          fluid={props.image}
+          style={{ height: '100%', width: '100%', position: 'relative' }}
+        ></Img>
+        <Text>{props.title}</Text>
       </CardFront>
       <CardBack
         style={{
           transform: transform.interpolate(t => `${t} rotateY(180deg)`),
         }}
       >
-        This is the back of the card.
+        <p>information here</p>
       </CardBack>
-    </StyledCard>
+    </StyledDiv>
   );
 };
 
-const StyledCard = styled.div`
+const Card = styled(animated.div)`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  cursor: pointer;
+  will-change: transform;
+  background-size: cover;
+  backface-visibility: hidden;
+  border-radius: 8px;
+  overflow: hidden;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const CardFront = styled(Card)``;
+
+const CardBack = styled(Card)`
+  padding: 1rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: var(--primaryDark);
+  @media (min-width: 920px) {
+    padding: 2.5rem;
+  }
+`;
+
+const StyledDiv = styled.div`
   width: 100%;
   height: 80px;
+  margin: 0 2rem 0 2rem;
   display: flex;
   align-items: center;
   justify-content: center;
+  position: relative;
   cursor: pointer;
-  will-change: transform;
   @media (max-width: 499px) {
     &:not(:last-of-type) {
       margin-bottom: 1.2rem;
@@ -71,30 +99,20 @@ const StyledCard = styled.div`
   }
 `;
 
-const StyledBackground = styled(BackgroundImage)`
+const Text = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
   height: 100%;
   width: 100%;
-  padding: 1rem;
   display: flex;
   justify-content: center;
   align-items: center;
-  position: absolute;
-  border-radius: 8px;
-  overflow: hidden;
-  backface-visibility: hidden;
-  @media (min-width: 920px) {
-    padding: 2.5rem;
-  }
-`;
-
-const CardFront = a(StyledBackground);
-
-const Text = styled.h3`
+  padding: 1rem;
+  background: rgba(0, 0, 0, 0.5);
+  margin: 0;
   text-align: center;
   text-transform: capitalize;
-  margin: 0;
-  align-self: center;
-
   @media (min-width: 500px) {
     font-size: 1.3em;
   }
@@ -103,22 +121,6 @@ const Text = styled.h3`
   }
   @media (min-width: 900px) {
     font-size: 1.8em;
-  }
-`;
-
-const CardBack = styled(a.div)`
-  height: 100%;
-  width: 100%;
-  padding: 1rem;
-  border-radius: 8px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  overflow: hidden;
-  position: absolute;
-  backface-visibility: hidden;
-  background: var(--primaryDark);
-  @media (min-width: 920px) {
     padding: 2.5rem;
   }
 `;
