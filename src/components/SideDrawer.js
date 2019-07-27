c89f1379d4dc768dcb09d8a57f00c3ce45328023
Andrew Scholz <yoitsscholzy@gmail.com>
2019-07-26 21:31:05 -0400

    fix drawer not closing on link click

--- src/components/SideDrawer.js
+++ src/components/SideDrawer.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import { useSpring, animated } from 'react-spring';
+import { useSpring, animated, config } from 'react-spring';
 
 import Navigation from './Navigation';
 
@@ -9,6 +9,7 @@ import { FaTimes } from 'react-icons/fa';
 const SideDrawer = ({ click, open }) => {
   const toggle = useSpring({
     transform: open ? 'translateX(0)' : 'translateX(105%)',
+    config: { clamp: true, mass: 1, tension: 120, friction: 16 },
   });
   return (
     <StyledSideDrawer style={toggle}>
