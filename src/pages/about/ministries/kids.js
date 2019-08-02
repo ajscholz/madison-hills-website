ffbf3f74c6e1f0821f134b5392c3f891275bc946
Andrew Scholz <yoitsscholzy@gmail.com>
2019-08-02 10:32:24 -0400

    update paths to components

--- src/pages/about/ministries/kids.js
+++ src/pages/about/ministries/kids.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { graphql } from 'gatsby';
 
-import HeroImage from '../../components/HeroImage';
-import Banner from '../../components/Banner';
-import Section from '../../components/Section';
+import HeroImage from '../../../components/HeroImage';
+import Banner from '../../../components/Banner';
+import Section from '../../../components/Section';
 
 export const data = graphql`
   {
