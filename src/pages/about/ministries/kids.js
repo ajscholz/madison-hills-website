e99afb119b40c9b1ec9c488d19cf31fc86e4943f
Andrew Scholz <yoitsscholzy@gmail.com>
2019-08-08 10:29:22 -0400

    update kids page to source regular ministries from contentful

--- src/pages/about/ministries/kids.js
+++ src/pages/about/ministries/kids.js
@@ -19,24 +19,27 @@ export const data = graphql`
         }
       }
     }
-    nursery: file(name: { eq: "nursery" }) {
-      image: childImageSharp {
-        fluid {
-          ...GatsbyImageSharpFluid
-        }
-      }
-    }
-    preK: file(name: { eq: "pre-k" }) {
-      image: childImageSharp {
-        fluid {
-          ...GatsbyImageSharpFluid
-        }
-      }
-    }
-    firstFifth: file(name: { eq: "1st-5th" }) {
-      image: childImageSharp {
-        fluid {
-          ...GatsbyImageSharpFluid
+    programs: allContentfulPrograms(
+      filter: { ministryArea: { eq: "Kids" } }
+      sort: { fields: createdAt, order: ASC }
+    ) {
+      edges {
+        program: node {
+          id
+          title
+          dayOfWeek
+          startTime
+          endTime
+          location
+          ageRange
+          description {
+            description
+          }
+          image {
+            fluid {
+              ...GatsbyContentfulFluid
+            }
+          }
         }
       }
     }
@@ -49,67 +52,26 @@ export default ({ data }) => {
       <Seo title="Madison Hills Kids Ministry" image={data.hero.image.src} />
       <HeroImage image={data.hero.image.fluid}>Kids Ministry</HeroImage>
 
-      <Section>
-        <Title>Nursery</Title>
-        <MinistrySection>
-          <Image fluid={data.nursery.image.fluid} />
-          <MinistryInfo>
-            <div style={{ fontWeight: 'bold' }}>
-              Location: Main level. Off main lobby
-            </div>
-            <div style={{ fontWeight: 'bold' }}>Ages: Lit'l ones</div>
-          </MinistryInfo>
-          <MinistryDescription>
-            We have a wonderful nursery for babies and young children, staffed
-            with loving and compassionate adults! Our nursery is brand new, with
-            age appropriate toys and a comfortable environment for the youngest
-            children.
-          </MinistryDescription>
-        </MinistrySection>
-      </Section>
-
-      <Section dark>
-        <Title>Preschool {`&`} Kindergarten</Title>
-        <MinistrySection flip>
-          <Image fluid={data.preK.image.fluid} />
-          <MinistryInfo>
-            <div style={{ fontWeight: 'bold', color: 'var(--primaryLight)' }}>
-              Location: Basement level
-            </div>
-            <div style={{ fontWeight: 'bold', color: 'var(--primaryLight)' }}>
-              Ages: 3 years - Kindergarten
-            </div>
-          </MinistryInfo>
-          <MinistryDescription>
-            This is where children are first introduced to the Bible, in the
-            most simplistic and innocent ways. We have two newly renovated
-            classrooms set up for children more suited for a learning
-            experience, led by one of the sweetest teachers! Kim Masters has
-            been leading this class for over 10 years and truly loves to teach
-            bright-eyed children about the love that God has for the world!
-          </MinistryDescription>
-        </MinistrySection>
-      </Section>
-
-      <Section>
-        <Title>{`1st - 5th Grade`}</Title>
-        <MinistrySection>
-          <Image fluid={data.firstFifth.image.fluid} />
-          <MinistryInfo>
-            <div style={{ fontWeight: 'bold' }}>Location: Basement level</div>
-            <div style={{ fontWeight: 'bold' }}>Ages: First - fifth grade</div>
-          </MinistryInfo>
-          <MinistryDescription>
-            Children that are in school will love the experiences they have when
-            they arrive for their weekly program! Students will learn more about
-            the Bible, while singing great songs and playing exciting games. We
-            have three age specific rooms set up for our school age children;
-            The Pet Shop, The Warehouse, and the Game Room! Steve and Terry
-            Marino have loved and ministered to school age children for over 7
-            years and are truly are a special couple!
-          </MinistryDescription>
-        </MinistrySection>
-      </Section>
+      {data.programs.edges.map(({ program }, index) => (
+        <Section dark={index % 2 === 1} key={program.id}>
+          <Title>{program.title}</Title>
+          <MinistrySection>
+            <Image fluid={program.image.fluid} />
+            <MinistryInfo>
+              <div style={{ fontWeight: 'bold' }}>
+                {program.dayOfWeek} {program.startTime}-{program.endTime}
+              </div>
+              <div style={{ fontWeight: 'bold' }}>
+                Location: {program.location}
+              </div>
+              <div style={{ fontWeight: 'bold' }}>Ages: {program.ageRange}</div>
+            </MinistryInfo>
+            <MinistryDescription>
+              {program.description.description}
+            </MinistryDescription>
+          </MinistrySection>
+        </Section>
+      ))}
 
       <Section dark>
         <Title>Stay In the Loop</Title>
