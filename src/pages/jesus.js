5a145b1ba707b9884bce191b611ea016da0a2453
Andrew Scholz <yoitsscholzy@gmail.com>
2019-08-29 20:46:13 -0400

    update contentful data structure

--- src/pages/jesus.js
+++ src/pages/jesus.js
@@ -10,21 +10,51 @@ import Button from '../components/Button';
 import { Link } from 'gatsby';
 import Img from 'gatsby-image';
 import ContentfulRichText from '../components/ContentfulRichText';
+import VerseSection from '../components/VerseSection';
+
+import { sectionHelper } from '../utils/helpers';
 
 const JesusPage = ({ data }) => {
-  const { image, sections } = data.page;
-
-  const s1 = sections[0];
-  const s2 = sections[1];
-  const verse = { content: [s2.content.json.textContent[0]] };
-  const reference = { content: [s2.content.json.textContent[1]] };
-  const s3 = sections[2];
+  const { image } = data.page;
 
   const img = {
     src: image.file.url,
     height: image.file.details.image.height,
     width: image.file.details.image.width,
   };
+
+  let sections = sectionHelper(
+    [
+      '7azFM2Q72liNatcMvjBGX1',
+      '2IKRWW43MC43QBIiqwY3QH',
+      '5GRCwmcPxNKWEpZSKE2HiG',
+    ],
+    data.page.sections
+  );
+
+  sections[0].content = (
+    <ContentfulRichText content={sections[0].textContent.json} />
+  );
+
+  let content = sections[1].contentReferences[0];
+  sections[1].content = (
+    <VerseSection text={content.text.text} reference={content.reference} />
+  );
+
+  sections[2].content = (
+    <GridContainer>
+      <Image
+        fluid={sections[2].image.fluid}
+        imgStyle={{ objectPosition: '57% 15%' }}
+      />
+      <MinistryDescription>
+        <ContentfulRichText content={sections[2].textContent.json} />
+        <Button>
+          <Link to="/visit">Plan A Visit</Link>
+        </Button>
+      </MinistryDescription>
+    </GridContainer>
+  );
 
   return (
     <>
@@ -36,146 +66,63 @@ const JesusPage = ({ data }) => {
         title="Jesus"
       />
 
-      <SectionOne>
-        <Title>{s1.title}</Title>
-        <ContentfulRichText content={s1.content.json} />
-      </SectionOne>
-
-      <SectionTwo>
-        <ContentfulRichText content={verse} className="verse" />
-        <ContentfulRichText content={reference} className="reference" />
-      </SectionTwo>
-
-      <SectionThree>
-        <Title>{s3.title}</Title>
-        <div className="text-content">
-          <ContentfulRichText content={s3.content.json} />
-        </div>
-        <Img fluid={s3.image.fluid} />
-        <Button>
-          <Link to="/visit">Plan A Visit</Link>
-        </Button>
-      </SectionThree>
+      {sections.map((section, i) => {
+        return (
+          <Section key={section.id}>
+            {i !== 1 && <Title>{section.title}</Title>}
+            {section.content}
+          </Section>
+        );
+      })}
     </>
   );
 };
 
-const SectionOne = styled(Section)`
-  text-align: center;
-  max-width: 900px;
-  margin: 0 auto;
-  & > p {
-    margin-top: 0;
-  }
-`;
-
-const SectionTwo = styled(Section)`
-  font-size: 1.5rem;
-  text-align: center;
-
-  & .verse {
-    margin: 0;
-    @media (min-width: 576px) {
-      font-size: 1.2em;
-    }
-    @media (min-width: 776px) {
-      font-size: 1.5em;
-    }
-  }
-
-  & .reference {
-    margin-bottom: 0;
-    color: var(--primary);
-    font-size: 0.7em;
-    @media (min-width: 576px) {
-      font-size: 0.85em;
-    }
-    @media (min-width: 776px) {
-      font-size: 1em;
-    }
-  }
-`;
-
-const SectionThree = styled(Section)`
+const GridContainer = styled.div`
   display: grid;
-  position: relative;
-  text-align: center;
-  max-width: 900px;
-  margin: 0 auto;
-  grid-template-columns: 100%;
-  grid-template-rows: auto;
+  justify-items: center;
+  grid-template-rows: 200px auto;
   grid-template-areas:
-    'title'
-    'text'
-    'button';
+    'picture'
+    'description';
+  grid-gap: 1rem;
+  width: 100%;
+  max-width: 700px;
+  text-align: center;
 
-  & ${Title} {
-    grid-area: title;
-    margin: 0 auto 2.5rem;
-    z-index: 1;
-  }
-
-  & .text-content {
-    grid-area: text;
-    z-index: 1;
-    & p {
-      margin-top: 0;
-    }
-  }
-
-  & button {
-    grid-area: button;
-    margin: 1rem auto 0;
-    z-index: 1;
-  }
-
-  & .gatsby-image-wrapper {
-    width: 100%;
-    height: 100%;
-    position: absolute !important;
-    top: 0;
-    left: 0;
-    background: var(--white);
-    & img {
-      opacity: 0.3 !important;
-      object-position: 58% 40% !important;
-    }
-  }
-
-  @media (min-width: 662px) {
-    grid-template-areas:
-      'title title'
-      'image text'
-      'image button';
+  @media (min-width: 660px) {
+    max-height: 250px;
     grid-template-columns: 40% 1fr;
-    grid-template-rows: repeat(3, auto);
-    grid-gap: 2rem;
+    grid-template-rows: 250px;
+    grid-template-areas: 'picture description';
+    grid-column-gap: 2rem;
+    justify-items: start;
+  }
+`;
 
-    & ${Title} {
-      margin: 0 auto 1rem;
+const Image = styled(Img)`
+  grid-area: picture;
+  width: 100%;
+  height: 100%;
+  margin-bottom: 0;
+  max-width: 400px;
+  /* object-fit: contain; */
+`;
+
+const MinistryDescription = styled.div`
+  & > p {
+    font-size: 0.9rem;
+    grid-area: description;
+    margin-top: 0;
+    @media (min-width: 660px) {
+      text-align: left !important;
     }
+  }
 
-    & .text-content {
-      text-align: left;
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      align-items: flex-start;
-      & p:last-of-type {
-        margin-bottom: 0;
-      }
-    }
-
-    & button {
-      margin: -0.5rem auto 0 0;
-    }
-
-    & .gatsby-image-wrapper {
-      position: static;
-      grid-area: image;
-      & img {
-        opacity: 1 !important;
-      }
+  & > button {
+    margin: 1.5rem auto 0 auto;
+    @media (min-width: 660px) {
+      margin: 1.5rem 0 0 0;
     }
   }
 `;
@@ -198,6 +145,7 @@ export const data = graphql`
         }
       }
       sections: section {
+        id: contentful_id
         title
         textContent {
           json
@@ -205,6 +153,14 @@ export const data = graphql`
         image {
           fluid {
             ...GatsbyContentfulFluid
+          }
+        }
+        contentReferences {
+          ... on ContentfulVerse {
+            reference
+            text {
+              text
+            }
           }
         }
       }
